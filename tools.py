41f761ca88576f9e6c3008bf44c156f4290b77ef
Timilehin Olubodun <100211336+Timmosky@users.noreply.github.com>
2025-06-29 13:41:26 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from langchain.tools.tavily_search import TavilySearchResults
@@ -18,15 +18,11 @@ save_tool = Tool(
     func=save_to_txt,
     description="Saves structured research data to a text file.",
 )
+tavily_api_key = st.secrets["tavily"]["api_key"]
+
+tavily_tool = TavilySearchResults(api_key=tavily_api_key)
 
 
-search = DuckDuckGoSearchRun()
-search_tool = Tool(
-
-    name = "search_web",
-    func=search.run,
-    description="Search the web for information",
-)
 
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
 wiki_tool = WikipediaQueryRun(api_wrapper= api_wrapper)
